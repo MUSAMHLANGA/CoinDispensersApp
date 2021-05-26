0954c0d618fc064c9c886c2c687c6bfd5324ff16
Lozililo Cathrine Sefako <cathrine.sefako@absa.africa>
2021-05-26 14:59:40 +0200

    Initial commit

---
+++ src/main/java/com/coinDispenser/CoinDispenserApp/CoinDispenserAppApplication.java
@@ -0,0 +1,13 @@
+package com.coinDispenser.CoinDispenserApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinDispenserAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinDispenserAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coinDispenser/CoinDispenserApp/CoinDispenserAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coinDispenser.CoinDispenserApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinDispenserAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
